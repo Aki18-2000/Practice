d6f5468624033acb79033a4a393211edb5ff0bb4
Aki18-2000 <makshaya206@gmail.com>
2025-03-18 19:25:36 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,25 +1,22 @@
 data "aws_availability_zones" "available" {}
 
-
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
-   tags = {
+  tags = {
     Name = "main-vpc"
   }
 }
 
 # Create Public Subnets
 resource "aws_subnet" "public1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet-1"
   }
 }
-
-
 
 # Create Internet Gateway
 resource "aws_internet_gateway" "igw" {
@@ -29,7 +26,6 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-
 # Create Route Tables
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
@@ -38,24 +34,17 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-   tags = {
+  
+  tags = {
     Name = "public-route-table"
   }
 }
-
-
 
 # Associate Route Tables with Subnets
 resource "aws_route_table_association" "public1" {
   subnet_id      = aws_subnet.public1.id
   route_table_id = aws_route_table.public.id
 }
-
-
-
-
-
-
 
 # Create Security Groups
 resource "aws_security_group" "web_sg" {
@@ -68,11 +57,11 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-   ingress {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -82,21 +71,19 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-   tags = {
+  tags = {
     Name = "web-sg"
   }
 }
 
-
 # Create EC2 Instances for Web Servers with Database Connection Example
 resource "aws_instance" "web1" {
-  ami           = "ami-053a45fff0a704a47"
-  instance_type = "t2.micro"
-  #availability_zone    = "us-east-1a" 
-  subnet_id     = aws_subnet.public1.id
-  security_groups = [aws_security_group.web_sg.id]
+  ami               = "ami-053a45fff0a704a47"
+  instance_type     = "t2.micro"
+  subnet_id         = aws_subnet.public1.id
+  security_groups   = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "web-server-1"
   }
-
+}
